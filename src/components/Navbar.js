6ed4aefeb8841c38800c9f17dd4715c72a25a607
BrainJarProductions <vgrilli1@gmail.com>
2019-12-09 19:16:06 -0500

    edit product page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -41,7 +41,7 @@ const Navbar = class extends Component {
         <div className='container'>
           <div className='navbar-brand'>
             <Link to='/' className='navbar-item' title='Logo'>
-              <img src={logo} alt='EOP' style={{ width: '85px' }} />
+              <img src={logo} alt='EOP' style={{ width: '80px' }} />
             </Link>
             {/* Hamburger menu */}
             <div
